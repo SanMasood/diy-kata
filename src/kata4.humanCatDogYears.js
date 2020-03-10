dfaa682c935436d9d56902680c9cdc7f7465127f
SanMasood <sanamasood.giki@gmail.com>
2020-03-10 12:19:32 +0000

    HumanCatDogYears done but could do with better design

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,24 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+
+    let array  = [];
+//let num= 90;
+let catYears= 16 + 4*number;
+let dogYears = 14 + 5*number;
+//console.log (num, catYears, dogYears);
+
+array.push(number);
+array.push(catYears);
+array.push(dogYears);
+return (`[${array}]`);
+/*or:
+function pusher(arrayItem,array){
+  return array.push(arrayItem);
+}
+pusher(num, array);
+pusher(catYears, array);
+pusher(dogYears, array);
+console.log (`[${array}]`);
+*/
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,14 @@
 const { humanCatDogYears } = require("../src");
 
+describe("humanCatDogYears", () => {
+    it("returns an array like this: [human-years, Cat-Years, Dog-Years]", () => {
+
+        expect(humanCatDogYears(13)).toBe("[13,68,79]");
+        expect(humanCatDogYears(56)).toBe('[56,240,294]');
+        expect(humanCatDogYears(90)).toBe('[90,376,464]');
+
+
+    });
+});
+
 // Look Ma, no handlebars!!!
