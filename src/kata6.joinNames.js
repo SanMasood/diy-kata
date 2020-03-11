cbff92dd2ff3663241a502c24245bfa470c7451c
SanMasood <sanamasood.giki@gmail.com>
2020-03-11 11:54:01 +0000

    Works for one name too

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,16 +1,17 @@
 const joinNames = namesObj => {
+  const strarray = [];
 
-    let strarray = [];
-    for(let i = 0; i<namesObj.length-1; i++)
-    {
-       strarray.push(namesObj[i].name);
-     }
-    let finalString = strarray.join(', ');
-    let lastString = namesObj[namesObj.length-1].name;
+  if (namesObj.length == 1)
+    return (namesObj[0].name);
 
-return (finalString+ " & " +lastString);
-//I wonder whether lines of code takes precedence over code readability 
-    
+  for (let i = 0; i < namesObj.length - 1; i++) {
+    strarray.push(namesObj[i].name);
+  }
+  const finalString = strarray.join(", ");
+  const lastString = namesObj[namesObj.length - 1].name;
+
+  return `${finalString} & ${lastString}`;
+  // I wonder whether lines of code takes precedence over code readability
 };
 
 module.exports = joinNames;
