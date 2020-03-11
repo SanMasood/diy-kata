e199b541b803d9e0733a11dae525fe30e0990af8
SanMasood <sanamasood.giki@gmail.com>
2020-03-11 09:51:43 +0000

    Joined names successfully

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,16 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+
+    let strarray = [];
+    for(let i = 0; i<namesObj.length-1; i++)
+    {
+       strarray.push(namesObj[i].name);
+     }
+    let finalString = strarray.join(', ');
+    let lastString = namesObj[namesObj.length-1].name;
+
+return (finalString+ " & " +lastString);
+
+    
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,13 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+
+    expect(joinNames(
+      [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toBe('Bart, Lisa & Maggie');
+
+      expect(joinNames(
+        [{name: 'Tom'}, {name: 'Harry'}, {name: 'William'}, {name: 'Philip'}])).toBe('Tom, Harry, William & Philip');
+
+  });
 });
