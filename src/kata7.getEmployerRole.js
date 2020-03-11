64a8f52f16dd3b24f7286a2b25ef06c5f3893c45
SanMasood <sanamasood.giki@gmail.com>
2020-03-11 10:47:23 +0000

    Got Employer Role

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,15 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+
+    for (let i=0; i < employees.length; i++) {
+        if (employees[i].name === employeeName) {
+            return employees[i].role;
+        }
+    }
+    /*const final = roleReturner('Sana', people);
+console.log (final);
+let convertToString = final.role; 
+console.log (convertToString);*/
+
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,13 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(
+      getEmployerRole("Lisa", [
+        { name: "Bart", role: "Big bro" },
+        { name: "Lisa", role: "Good Sister" },
+        { name: "Maggie", role: "Baby" }
+      ])
+    ).toBe("Good Sister");
+  });
 });
